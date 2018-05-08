de160cef96dd6a45c9c9c7f5ef5dff734ee30813
Vinicius Bloise <vinicius.bloise@gmail.com>
2018-05-08 14:25:11 +0200

    gulpfile

---
+++ CuoreRosso/gulpfile.js
@@ -0,0 +1,62 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+  rimraf = require("rimraf"),
+  concat = require("gulp-concat"),
+  cssmin = require("gulp-cssmin"),
+  uglify = require("gulp-uglify");
+
+var paths = {
+  webroot: "./wwwroot/",
+  theme: "theme/layout/",
+};
+
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+
+var module_path = {
+  node_modules: "./node_modules/",
+  themes: "./Themes/Default/",
+  materialize: {}
+}
+module_path.materialize.base = module_path.node_modules + "materialize-css/";
+module_path.materialize.js = module_path.materialize.base + "dist/**/*.js";
+module_path.materialize.css = module_path.materialize.base + "dist/**/*.css";
+
+
+gulp.task("clean:js", function (cb) {
+  rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+  rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+  return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+    .pipe(concat(paths.concatJsDest))
+    .pipe(uglify())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+  return gulp.src([paths.css, "!" + paths.minCss])
+    .pipe(concat(paths.concatCssDest))
+    .pipe(cssmin())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("copy-modules", function() {
+  return gulp.src([module_path.materialize.js, module_path.materialize.css])
+    .pipe(gulp.dest(module_path.themes))
+    .pipe(gulp.dest(paths.webroot + paths.theme));
+})
